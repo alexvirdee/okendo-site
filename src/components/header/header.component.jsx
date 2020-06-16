d14018bf51ca03cadd27a03f30d6137b65a4cea5
Alexinder Virdee <alex.virdee08@gmail.com>
2020-06-16 17:38:24 -0400

    Header component setup with steps added

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './header.styles.scss';
+
+const Header = () => (
+    <div className='header'>
+        <div className='header-text'>
+            <h1 className='title'>1815 Rose Gold Chronograph Watch - Brown Croco Strap</h1>
+            <h2 className='subtitle'>Review</h2>
+        </div>
+        <div className='nav-container'>
+            <div className='grid-outterGutter'>
+                <nav className='header-steps'>
+                    <div className='step'>
+                        <a href="#">Review</a>   
+                    </div>
+                    <div className='step'>
+                       
+                    <a href="#">Ratings</a>  
+                    </div>
+                    <div className='step'>
+                        
+                    <a href="#">Media</a>  
+                    </div>
+                    <div className='step'>
+                        
+                    <a href="#">Confirm</a>  
+                    </div>
+                </nav>
+            </div>
+        </div>
+   </div>
+)
+
+export default Header;
