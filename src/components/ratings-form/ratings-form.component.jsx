00e6293bcf1670673406ebef80133bcbebffdd99
Alexinder Virdee <alex.virdee08@gmail.com>
2020-06-17 13:31:42 -0400

    Ratings user information

--- src/components/ratings-form/ratings-form.component.jsx
+++ src/components/ratings-form/ratings-form.component.jsx
@@ -12,6 +12,7 @@ const RatingsForm = () => (
                             <div className='range-attributes'>
                                 <div className='quality'>
                                     <div className='range'>
+                                        {/* Main Product Ratings */}
                                         <label className='range-label'>Quality</label>
                                         <div className='c-numberRangeInput'>
                                             <input type='range' className='u-assistive-hidden' id='number-range-quality' value='0' step='1' max='5' />
@@ -111,8 +112,181 @@ const RatingsForm = () => (
                                     </div>
                                 </div>
                             </div>
-                            <div className='general-value-attributes'></div>
-                            <fieldset className='shopper-value-attributes'></fieldset>
+
+                            {/* Product Highlights  */}
+                            <div className='general-value-attributes'>
+                                <div className='value-attributes-wrapper'>
+                                    <div>Product Standouts</div>
+                                    <div className='options-text'>
+                                        <span> Choose up to 5 that best apply (optional)</span>
+                                    </div>
+                                    <div classNmae='option-inputs'>
+                                        <div className='option-inputs-item'>
+                                        <div className='option-input-control'>
+                                            <span>Accurate Timekeeping</span>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                                    <div classNmae='option-inputs'>
+                                    <div className='option-inputs-item'>
+                                        <div className='option-input-control'>
+                                            <span>High Quality</span>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                                    <div classNmae='option-inputs'>
+                                    <div className='option-inputs-item'>
+                                        <div className='option-input-control'>
+                                            <span>Durable</span>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                                    <div classNmae='option-inputs'>
+                                    <div className='option-inputs-item'>
+                                        <div className='option-input-control'>
+                                            <span>Elegant</span>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                                    <div classNmae='option-inputs'>
+                                    <div className='option-inputs-item'>
+                                        <div className='option-input-control'>
+                                            <span>Good Weight</span>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                                    <div classNmae='option-inputs'>
+                                    <div className='option-inputs-item'>
+                                        <div className='option-input-control'>
+                                            <span>Versatile</span>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                                    <div classNmae='option-inputs'>
+                                    <div className='option-inputs-item'>
+                                        <div className='option-input-control'>
+                                            <span>Looks Expensive</span>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                                    <div classNmae='option-inputs'>
+                                    <div className='option-inputs-item'>
+                                        <div className='option-input-control'>
+                                            <span>Attracts Compliments</span>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                                    <div classNmae='option-inputs'>
+                                    <div className='option-inputs-item'>
+                                        <div className='option-input-control'>
+                                            <span>Unique</span>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                                    <div classNmae='option-inputs'>
+                                    <div className='option-inputs-item'>
+                                        <div className='option-input-control'>
+                                            <span>Great Gift</span>
+                                        </div>
+                                        </div>
+                                    </div>
+
+                                    <div classNmae='option-inputs'>
+                                    <div className='option-inputs-item'>
+                                        <div className='option-input-control'>
+                                            <span>Great Value</span>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <fieldset className='shopper-value-attributes'>
+                                <legend>
+                                    <span>About You</span>
+                                </legend>
+
+                                {/* Age Range */}
+                                <div className='shopper-attributes'>
+                                    <div>Age Range</div>
+                                    <div>Choose one</div>
+                                    <div>
+                                        <div className='age-option-inputs'>
+                                            <div className='option-inputs-item'>
+                                                <div className='options-inputs-control'>
+                                                    <span>Under 18</span>
+                                                </div>
+                                            </div>
+
+                                            <div className='option-inputs-item'>
+                                                <div className='options-inputs-control'>
+                                                    <span>18 - 24</span>
+                                                </div>
+                                            </div>
+
+                                            <div className='option-inputs-item'>
+                                                <div className='options-inputs-control'>
+                                                    <span>25 - 34</span>
+                                                </div>
+                                            </div>
+
+                                            <div className='option-inputs-item'>
+                                                <div className='options-inputs-control'>
+                                                    <span>35 - 44</span>
+                                                </div>
+                                            </div>
+
+                                            <div className='option-inputs-item'>
+                                                <div className='options-inputs-control'>
+                                                    <span>45 - 54</span>
+                                                </div>
+                                            </div>
+
+                                            <div className='option-inputs-item'>
+                                                <div className='options-inputs-control'>
+                                                    <span>55 - 64</span>
+                                                </div>
+                                            </div>
+
+                                            <div className='option-inputs-item'>
+                                                <div className='options-inputs-control'>
+                                                    <span>65+</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    {/* Bought For Section */}
+                                    <div className='bought-for'>
+                                        <div>Bought For</div>
+                                        <div>Choose one</div>
+                                        <div>
+                                            <div className='option-inputs'>
+                                                <div className='option-input-item'>
+                                                    <div className='option-input-item-control'>
+                                                        <span>Personal Use</span>
+                                                    </div>
+                                                </div>
+                                                <div className='option-input-item'>
+                                                    <div className='option-input-item-control'>
+                                                        <span>Gift</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                            </fieldset>
                         </div>
                     </div>
                 </div>
