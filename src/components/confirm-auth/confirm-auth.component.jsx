b584c9c14026c139941de0c9420f68e3cf6c9804
Alexinder Virdee <alex.virdee08@gmail.com>
2020-06-18 11:25:31 -0400

    Confirm-auth component step

--- src/components/confirm-auth/confirm-auth.component.jsx
+++ src/components/confirm-auth/confirm-auth.component.jsx
@@ -7,9 +7,26 @@ const ConfirmAuth = () => {
         <main>
             <div className='u-container'>
                 <div className='gutter'>
-                    <div className='card'>
-                        
+                    <div className='card auth-card'>
+                    <form>
+                    <input type="hidden" name="returnUrl" value="" autocomplete="off" />
+                    <h2>Add your profile picture to your review!</h2>
+                    <p className='mb-medium'>This helps other shoppers by adding authenticity to your review.</p>
+                    <div className='profile-btns'>
+                        <button type='submit' name='provider' value='Google' id='Google' className='signIn signIn-google'>
+                            Connect with Google+
+                        </button>
+                        <button type='submit' name='provider' value='Facebook' id='Facebook' className='signIn signIn-facebook'>
+                            Connect with Facebook
+                        </button>
+                    </div>
+                    </form>
                     <div className='divider'><span>OR</span></div>
+                        <form>
+                            <div className='signIn signIn-guest'>
+                                <button className='signIn-guest-btn'>Continue as guest</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
