dceac84b019479004766215cae512e589f631d74
Alexinder Virdee <alex.virdee08@gmail.com>
2020-06-22 11:02:36 -0400

    Updates

---
+++ precache-manifest.c346b828e42c13930653c3d1105f73ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef568d3f4e7369637297dac547582a2b",
+    "url": "/okendo-site/index.html"
+  },
+  {
+    "revision": "82c5b8e228d74f25badd",
+    "url": "/okendo-site/static/css/main.7e367690.chunk.css"
+  },
+  {
+    "revision": "f8f1b09c222a93d8cfb0",
+    "url": "/okendo-site/static/js/2.670a1d25.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/okendo-site/static/js/2.670a1d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82c5b8e228d74f25badd",
+    "url": "/okendo-site/static/js/main.b60a9fa2.chunk.js"
+  },
+  {
+    "revision": "1fe6a65a862ec810d10c",
+    "url": "/okendo-site/static/js/runtime-main.1983425c.js"
+  }
+]);
